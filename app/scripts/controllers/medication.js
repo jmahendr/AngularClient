acfa86a31491c85326b202e355e09f8adddd1128
Joshua Mahendran <joshua.mahendran@gmail.com>
2017-08-28 15:29:09 +0530

    added ng-switch and ng-include to render medication page fragments

--- app/scripts/controllers/medication.js
+++ app/scripts/controllers/medication.js
@@ -2,8 +2,34 @@
 
 angular.module('mediApp')
 .controller('MedicationCtrl', ['$scope', '$stateParams', 'userService', function ($scope, $stateParams, userService) {
-    var userMedications = userService.getUserDrugs().query();
-   $scope.userMedications = userMedications;
+	
+	var selection = {};
+	console.log($scope.selection);
+	if(typeof $scope.selection === 'undefined'){
+		selectTab('home');
+	}
+		
     
-    console.log(userMedications);
+
+	function selectTab(setTab) {
+		if(setTab == 'home') {
+			var userMedications = userService.getUserDrugs().query();
+			$scope.userMedications = userMedications;
+			console.log(userMedications);
+		}
+		console.log("selectTab invoked with " + setTab);
+		$scope.selection = {selected: setTab};
+		console.log($scope.selection.selected);
+	};
+	
+	function isSelected(tab) {
+		return $scope.selection.selected == tab;
+	}
+	
+	$scope.isSelected = isSelected;
+	
+	$scope.selectTab = selectTab;
+	
+	
+	
 }]);
