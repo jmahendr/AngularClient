d9090b0cc5bb28909b2fbd9987860b2b020a5feb
Joshua Mahendran <joshua.mahendran@gmail.com>
2017-11-15 15:00:27 +0530

    impl login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,49 +1,59 @@
 'use strict';
 
-angular.module('mediApp', ['ui.router', 'ngResource'])
+angular.module('mediApp', ['ui.router', 'ngResource','ngDialog'])
 	.config(function($stateProvider, $urlRouterProvider){
 		$stateProvider
-            .state('app', {
-                url:'/',
-                views: {
+      .state('app', {
+				url:'/',
+				views: {
 					'header': {
-                        templateUrl : 'views/header.html'		
+						templateUrl : 'views/header.html',
+						controller: 'MainCtrl'
 					},
-                    'content': {
-                        templateUrl : 'views/main.html',
-                        controller  : 'MainCtrl'
-                    },
+					'content': {
+						templateUrl : 'views/main.html',
+						controller  : 'MainCtrl'
+					},
 					'footer': {
-                        templateUrl : 'views/footer.html'
-                    }
+						templateUrl : 'views/footer.html'
+					}
 				}
             })
 			.state('app.aboutus', {
-                url: 'aboutus',
-                views: {
-                    'content@': {
-                        template : '<h1> about us!</H1>'
-                    }
+				url: 'aboutus',
+				views: {
+					'content@': {
+						template : '<h1> about us!</H1>'
+					}
 				}
-            })
+			})
 			.state('app.contactus', {
-                url: 'contactus',
-                views: {
-                    'content@': {
-                        templateUrl : 'views/contact.html'
-                    }
+				url: 'contactus',
+				views: {
+					'content@': {
+						templateUrl : 'views/contact.html'
+					}
 				}
-            })
-            .state('app.medication', {
-                url: 'medication',
-                views: {
-                    'content@': {
-                        templateUrl : 'views/medication.html',
-                        controller  : 'MedicationCtrl'
-                    }
+			})
+      .state('app.medication', {
+				url: 'medication',
+				views: {
+					'content@': {
+						templateUrl : 'views/medication.html',
+						controller  : 'MedicationCtrl'
+					}
 				}
-            });
-	
-        $urlRouterProvider.otherwise('/');
+			})
+			.state('app.login',{
+				url: 'login',
+				views: {
+					'content@' : {
+						templateUrl : 'views/login.html',
+						controller: 'LoginCtrl'
+					}
+				}
+			});
+
+      $urlRouterProvider.otherwise('/');
 })
 ;
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,24 @@
 'use strict';
 angular.module('mediApp')
-.controller('MainCtrl', ['$scope', '$stateParams', 'userService', 
-                         function ($scope, $stateParams, userService) {
-  
-    //$scope.medications = [{drug:'a'},{drug:'d'},{drug:'f'},{drug:'b'}];
-    $scope.medications = userService.getUserDrugs().query();
+.controller('MainCtrl', ['$scope', '$stateParams', 'userService', 'ngDialog',
+                         function ($scope, $stateParams, userService, ngDialog) {
+
+  $scope.medications = userService.getUserDrugs().query();
 	console.log($scope.medications);
+
+  $scope.openLogin = function () {
+    ngDialog.open({ template: 'views/login.html', scope: $scope, className: 'ngdialog-content', controller:"MainCtrl" });
+  };
+  /*
+  $scope.logOut = function() {
+     AuthFactory.logout();
+      $scope.loggedIn = false;
+      $scope.username = '';
+  };
+
+  $rootScope.$on('login:Successful', function () {
+      $scope.loggedIn = AuthFactory.isAuthenticated();
+      $scope.username = AuthFactory.getUsername();
+  });
+  */
 }]);
